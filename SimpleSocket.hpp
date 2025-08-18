bc6ac96d25eed1fe08c7d6509ee318d1316ef6e8
3idey <elaidya225@gmail.com>
2025-08-18 19:22:17 +0300

    what the hell im doing here

--- SimpleSocket.hpp
+++ SimpleSocket.hpp
@@ -18,6 +18,9 @@ namespace HDE
         SimpleSocket(int domain, int service, int protocol, int port, u_long interface);
         virtual int connect_to_network(int sock, struct sockaddr_in address) = 0;
         void test_connection(int);
+        struct sockaddr_in get_address();
+        int get_sock();
+        int get_connection();
     };
 
 }
